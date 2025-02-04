8f9b62996c03d2f3c5822fde88397b3c6bb7f5eb
Justin Case <justin.case1061@gmail.com>
2025-02-03 23:40:32 -0600

    trimmed code

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -7,7 +7,7 @@ import { keyBy } from "../../utils/utils.js";
 import TrashIcon from "../../public/trash1.svg";
 
 const CartPage = () => {
-  const { cart, setCart, products, prices, cartAmount, setCartAmount } = useContext(ShopContext);
+  const { cart, setCart, products, prices } = useContext(ShopContext);
   const [cartTotal, setCartTotal] = useState(0);
 
   const fetchProductInfo = (item) => {
